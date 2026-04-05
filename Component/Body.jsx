5162525b412ac0293da0f96b161b1ecc750fc590
Moonknight <moonknight@gmail.com>
2026-04-06 00:18:17 +0530

    Fourth Commit

--- Component/Body.jsx
+++ Component/Body.jsx
@@ -1,23 +1,37 @@
-function Body(props){
+function Body(props) {
 
-const citywise=props?.cityData?.cities;
-    return(
+    const citywise = props?.cityData?.cities;
+    return (
         <>
-        <div className="BodyDiv1">
-           {citywise.map((city,index)=>
-                <div className="cardData" key={index}>
-                    <p>{city.section_title}</p>
+            <div className="BodyDiv1">
+                {citywise.map((city, index) =>
+                    <div className="cardData" key={index}>
+                        <p>{city.section_title}</p>
 
-                    {city.listings.map((item,i)=>
-                        <div className="card" key={i}>
-                            {item.title}
-                            {item.price}
+                        <div className="card-wrap">
+                            {city.listings.map((item, i) =>
+                                <div className="card" key={i}>
+                                    <div className="image">
+                                        <img src={item.image_url} alt="Property-img"></img>
+                                        <div className="rating">
+                                            <p>{item.rating}{"⭐"}</p>
+                                        </div>
+                                    </div>
+
+                                    <div className="card-title">
+                                        <p>{item.title}</p>
+                                    </div>
+
+                                    <div className="card-price">
+                                        <p>{item.price}{" "}{item.price_qualifier}</p>
+                                    </div>
+                                </div>
+                            )}
                         </div>
-                        )}
-                       
+
                     </div>
-            )}
-        </div>
+                )}
+            </div>
         </>
     )
 }
