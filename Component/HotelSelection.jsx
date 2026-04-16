bdc8ecbcd064a0e676fe599dd6a854f1c9f76d37
Moonknight <moonknight@gmail.com>
2026-04-16 20:02:39 +0530

    Updateapr16

--- Component/HotelSelection.jsx
+++ Component/HotelSelection.jsx
@@ -1,7 +1,8 @@
-import { id } from "date-fns/locale";
 
 
 function HotelSelect({ roomData }) {
+    const istrue = roomData?.badge;
+
     return (
         <>
 
@@ -37,6 +38,42 @@ function HotelSelect({ roomData }) {
                 <div className="Room-info">
                     <p>{`${roomData?.capacity.max_guests} guests - ${roomData?.capacity.bedrooms} bedrooms - ${roomData?.capacity.beds} beds - ${roomData?.capacity.bathrooms} bathrooms`}</p>
                 </div>
+
+                {istrue && (
+                    <div className="Roombadge">
+                        <div className="sidebanner">
+
+                            {/* LEFT */}
+                            <div className="sidebanner-left">
+                                <img src="https://a0.muscache.com/im/pictures/airbnb-platform-assets/AirbnbPlatformAssets-GuestFavorite/original/059619e1-1751-42dd-84e4-50881483571a.png?im_w=240" />
+                                <p>Guest favourite</p>
+                                <img src="https://a0.muscache.com/im/pictures/airbnb-platform-assets/AirbnbPlatformAssets-GuestFavorite/original/33b80859-e87e-4c86-841c-645c786ba4c1.png?im_w=240" />
+                            </div>
+
+                            {/* CENTER */}
+                            <div className="sidebanner-center">
+                                One of the most loved homes on Airbnb, according to guests
+                            </div>
+
+                            {/* RIGHT */}
+                            <div className="sidebanner-right">
+
+                                <div className="right-block">
+                                    <div className="rating-num">{roomData?.rating}</div>
+                                    <div className="stars">★★★★★</div>
+                                </div>
+
+                                <div className="right-block">
+                                    <div className="reviews-num">{roomData?.review_count}</div>
+                                    <div className="reviews-label">Reviews</div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                )}
+
             </div>
         </>
     )
